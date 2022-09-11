01155fef973c4e096e2bace63560ac29b35c9647
Uzoma <talk2uzilon@gmail.com>
2022-09-10 19:17:49 -0700

    0-positive_or_negative.c betty 3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main (void)
 {
 	int n;
 
-	srand(time(0));
+	SRAND(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
@@ -28,4 +28,4 @@ int main (void)
 		printf("%d is positive\n", n);
 	}
 	return (0);
-};
+}
