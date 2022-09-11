eb5cf06d028359f338630c448c00fb0500c036bb
Uzoma <talk2uzilon@gmail.com>
2022-09-11 14:06:18 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:always return o
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
