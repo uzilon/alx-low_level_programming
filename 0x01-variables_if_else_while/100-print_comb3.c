9ea9d9894c834d884ebbb363b0d8c084c3e2964e
Uzoma <talk2uzilon@gmail.com>
2022-09-11 17:00:14 -0700

    100-print_comb3.c correction2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,15 @@ int main(void)
 	{
 		for (j = 10; j <= 19; j++)
 		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 19 || j != 19)
+			if ((j % 10) > (i % 10))
 			{
-				putchar(',');
-				putchar(' ');
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
