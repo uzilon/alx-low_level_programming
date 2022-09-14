fd5bf4aab5efab47c56fd00a90ce197f62d0d527
Uzoma <talk2uzilon@gmail.com>
2022-09-13 20:31:29 -0700

    8-24_hours.c c1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,14 @@
 
 /**
  * jack_bauer - main thing
- * 
+ *
  */
 void jack_bauer(void)
 {
 	int i;
 	int j;
 
-	for (i = o; i <= 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
 		for (j = 0; i <= 59; j++)
 		{
