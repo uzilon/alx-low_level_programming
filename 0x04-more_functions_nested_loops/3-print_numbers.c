de714f44a2ca1bea3f0f4a6f004b71318bdd50c1
Uzoma <talk2uzilon@gmail.com>
2022-09-16 19:37:00 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the digits 1-9
+ */
+
+void print_numbers(void)
+{
+	int 1 = 0
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		++1
+	}
+	_putchar("\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,7 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
+void print_numbers(void);
+
 
 #endif
