6c48db5168fa816fc084e744fe75d984d1f52e84
Uzoma <talk2uzilon@gmail.com>
2022-09-19 18:54:02 -0700

    7-puts_half.cc2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,17 +14,10 @@ void puts_half(char *str)
 	x = strlen(str);
 
 	if (x % 2 == 1)
-	{
 		y = x / 2 + 1;
-	}
 	else
-	{
 		y = x / 2;
-	}
-
 	for (i = y; i < x; i++)
-	{
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
