9b37c0d7c95f2b2a5948fc00517b11b081933148
Uzoma <talk2uzilon@gmail.com>
2022-09-22 08:44:00 -0700

    2-strncpy.c5

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i]
-	for ( ; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
 		dest[i] = '\0';
-
+	}
 	return (dest);
 }
