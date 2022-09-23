852ae9526c0bd99252cf071fc22ee7a00c694644
Uzoma <talk2uzilon@gmail.com>
2022-09-22 21:40:34 -0700

    6-string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,7 +27,7 @@ char *cap_string(char *str)
 
 	while (str[++ index])
 	{
-		while (!(str[index] >= 'a' && (str[index] <= 'z'))
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
@@ -41,7 +41,7 @@ char *cap_string(char *str)
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
-				str[index - 1] == '}'
+				str[index - 1] == '}')
 			str[index] -= 32;
 	}
 	return (str);
