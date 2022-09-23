6f50a2268c879994e9600e31c21a4a9d1c4b9087
Uzoma <talk2uzilon@gmail.com>
2022-09-22 21:19:53 -0700

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,20 @@
 
 /**
  * string_toupper - changes all lowercase letters to uppercase
- * @s: an input string
+ * @str: string to be changed
  *
  * Return: char pointer to converted string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-	char *str = s;
+	int i = 0;
 
-	while (*s)
+	while (str[i] != '\0')
 	{
-		if (*s >= 'a' && *s <= 'z')
-			*s -= 32;
-		s++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
