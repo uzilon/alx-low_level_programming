7e4c0e7b48f8f6f508fe89991d2e9ac3b6131d94
Uzoma <talk2uzilon@gmail.com>
2022-09-26 15:33:53 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates character in string
+ * @s: string to locate character
+ * @c: character to find
+ * Return: character value
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
