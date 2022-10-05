48ee190a083c9fac0a4c2e86554f7b0c52113311
Uzoma <talk2uzilon@gmail.com>
2022-10-04 18:10:36 -0700

    malloc7

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - concat all arguments into string
+ * @ac: Number of argument
+ * @av: An array of pointers to the arguments
+ * Return: NULL otherwise - a pointer to the new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, i, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[i++] = av[arg][byte];
+
+		str[i++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}
