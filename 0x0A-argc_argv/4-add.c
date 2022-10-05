06fa95a064dccc39c290faad3ee1992584c6431b
Uzoma <talk2uzilon@gmail.com>
2022-10-04 18:24:07 -0700

    4-add1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,59 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#define UNUSED(x) (void)(x)
-/**
- * StringCheck  - checks string
- * @s: string to check
- * Return: boolean
- */
 
-int StringCheck(char *s)
-{
-	int i = 0;
-
-	for (; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- * Return: always 0
+ * main - Prints the addition of positive numbers,
+ * followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: If one of the numbers contains symbols that are non-is - 1.
+ * Otherwise - 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
+	int n, i, sum = 0;
 
-	if (argc > 1)
+	for (n = 1; n < argc; n++)
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 0; argv[n][i]; i++)
 		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
+			if (argv[n][i] < '0' || argv[n][i] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		printf("%d\n", result);
-			return (0);
+
+		sum += atoi(argv[n]);
 	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
