57ad44c0c83598e8ef022ffdfa8d0e3d1ec87fd0
Uzoma <talk2uzilon@gmail.com>
2022-10-12 17:26:36 -0700

    ftopointerc4

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN
-#define MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -7,4 +7,4 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif
+#endif /* MAIN_H */
